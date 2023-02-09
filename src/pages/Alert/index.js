7169672ec074fa9b7b843fc8d402e5470df71e2d
Bianca Hotops Alves <48944941831>
2023-02-09 08:17:03 -0300

    Alert

--- src/pages/Alert/index.js
+++ src/pages/Alert/index.js
@@ -8,6 +8,11 @@ export default function Alert() {
     console.log('orientation is:', orientation)
     return (
         <View style={styles.container}>
+          <View style={styles.barras}>
+            <View style={styles.barra1} />
+            <View style={styles.barra2} />
+            <View style={styles.barra3} />
+          </View>
         </View>
 
 );
@@ -16,5 +21,28 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         backgroundColor: '#38a699'
+    },
+
+    barras:{
+      flex:1,
+      justifyContent:'center',
+      alignItems: 'center',
+    },
+
+    barra1:{
+      width: 50,
+      height: 50,
+      backgroundColor: 'powderblue',
+    },
+    barra2:{
+      width: 100,
+      height: 100,
+      backgroundColor: 'skyblue',
+    },
+
+    barra3:{
+      width: 150,
+      height: 150,
+      backgroundColor: 'steelblue',
     },
 })
